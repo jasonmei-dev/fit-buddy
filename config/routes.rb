8ec4178f0b3e10ae61d03fdf846b61700be30715
Jason Mei <jason.mei403@gmail.com>
2019-06-15 14:46:20 -0400

    create Sessions controller and new sessions form

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   resources :exercises
   resources :workouts
   resources :users
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
