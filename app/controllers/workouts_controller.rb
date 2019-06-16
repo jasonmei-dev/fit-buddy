702df8c26937bee2148a6dab1e3c8b8f60726476
Jason Mei <jason.mei403@gmail.com>
2019-06-16 00:10:43 -0400

    add Workout new, create, show actions and views

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,2 +1,25 @@
 class WorkoutsController < ApplicationController
+  def new
+    @workout = Workout.new
+  end
+
+  def create
+    @workout = current_user.workouts.new(workout_params)
+    if @workout.save
+      redirect_to workout_path(@workout)
+    else
+      redirect_to new_workout_path
+    end
+  end
+
+  def show
+    @workout = current_user.workouts.find(params[:id])
+    binding.pry
+  end
+
+  private
+
+  def workout_params
+    params.require(:workout).permit(:name, :duration_mins, :share_status)
+  end
 end
