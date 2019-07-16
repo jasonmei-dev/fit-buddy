a0b40806d7d1b17588a8292d0226adbbeca5fe13
Jason Mei <jason.mei403@gmail.com>
2019-07-16 02:16:28 -0400

    add flash message to user controller and refactor user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
+      flash[:success] = "Sign Up and Log In successful!"
       redirect_to user_path(@user)
     else
       render :new
