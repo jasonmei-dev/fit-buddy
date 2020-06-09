f80517f67809591cb9254d65b904b8e73514a6ca
jmei403 <jason.mei403@gmail.com>
2020-06-09 10:27:18 -0400

    refactor

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,12 @@ class SessionsController < ApplicationController
   end
 
   def create #login
-    if auth_hash = request.env["omniauth.auth"]
+    if auth_hash #login via OAuth
       @user = User.find_or_create_by_omniauth(auth_hash)
       session[:user_id] = @user.id
       flash[:success] = "Login successful!"
       redirect_to root_path
-    else
+    else #normal login
       @user = User.find_by(username: params[:user][:username])
       if @user && @user.authenticate(params[:user][:password])
         session[:user_id] = @user.id
@@ -26,4 +26,9 @@ class SessionsController < ApplicationController
     session.delete :user_id
     redirect_to root_path
   end
+
+  protected
+    def auth_hash
+      request.env["omniauth.auth"]
+    end
 end
