2a1928a934bdcaf396fcf5c167eff01ccd5bb485
jmei403 <jason.mei403@gmail.com>
2020-06-09 10:29:30 -0400

    update auth callback route

--- config/routes.rb
+++ config/routes.rb
@@ -3,19 +3,20 @@ Rails.application.routes.draw do
   resources :exercises
   resources :workouts
   resources :users
-
+  
   resources :users, only: [:show] do
     resources :workouts, only: [:show, :new, :edit, :destroy]
   end
-
+  
   resources :workouts, only: [:show] do
     resources :exercise_entries, only: [:new, :edit, :destroy]
   end
-
+  
+  # get '/auth/:provider/callback' => 'sessions#create'
+  match '/auth/:provider/callback' => 'sessions#create', via: [:get, :post]
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
-  get '/auth/:provider/callback' => 'sessions#create'
 
   root 'application#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
