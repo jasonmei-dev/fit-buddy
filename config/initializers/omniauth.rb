c993e974dffaa34774952c8579d7e619f3d62f77
jmei403 <jason.mei403@gmail.com>
2020-06-15 10:25:33 -0400

    update facebook scope

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
    provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
-   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
+      scope: 'public_profile, email', info_fields: 'email, first_name, last_name'
    provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET']
 end
