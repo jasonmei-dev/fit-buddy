09f2ee788052b02a4244827d2ec8431c6c7c7c01
jmei403 <jason.mei403@gmail.com>
2020-06-16 14:09:56 -0400

    replace username login with email login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
       flash[:success] = "Login successful!"
       redirect_to root_path
     else #normal login
-      @user = User.find_by(username: params[:user][:username])
+      @user = User.find_by(email: params[:user][:email])
       if @user && @user.authenticate(params[:user][:password])
         session[:user_id] = @user.id
         flash[:success] = "Login successful!"
